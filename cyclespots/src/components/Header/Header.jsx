f7740c1258b4f8711ffeba3ebcf153aef6c8ce8a
Aubrey Garcia <aubreymaerg@gmail.com>
2026-03-13 19:50:02 -0400

    Updated nav menu icon behavior

--- cyclespots/src/components/Header/Header.jsx
+++ cyclespots/src/components/Header/Header.jsx
@@ -21,40 +21,66 @@ function Menu() {
             </svg>
             <h1 className="nav__title-name">CycleSpots</h1>
           </div>
-          <svg
-            width="24px"
-            height="24px"
-            stroke-width="1.5"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="menu-icon"
+          <div
+            id="nav__icons"
             onClick={() => {
               setMenuOpen(!menuOpen);
             }}
           >
-            <path
-              d="M3 5H21"
-              stroke="#000000"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></path>
-            <path
-              d="M3 12H21"
-              stroke="#000000"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></path>
-            <path
-              d="M3 19H21"
-              stroke="#000000"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></path>
-          </svg>
+            {menuOpen ? (
+              <svg
+                width="24px"
+                height="24px"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                id="menu-icon-x"
+                className={menuOpen ? "open" : ""}
+              >
+                <path
+                  d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
+                  stroke="#000000"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                width="24px"
+                height="24px"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                id="menu-icon"
+                className={menuOpen ? "open" : ""}
+              >
+                <path
+                  d="M3 5H21"
+                  stroke="#000000"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  d="M3 12H21"
+                  stroke="#000000"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  d="M3 19H21"
+                  stroke="#000000"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            )}
+          </div>
         </div>
         <ul id="nav__menu" className={menuOpen ? "open" : ""}>
           <li className="nav__menu-item">
