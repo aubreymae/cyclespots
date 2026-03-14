f94ef927951e76c5f56378060b56a4a88a8274a7
Aubrey Garcia <aubreymaerg@gmail.com>
2026-03-14 15:19:09 -0400

    Create SearchListing page

---
+++ cyclespots/src/components/SearchListing/SearchListing.jsx
@@ -0,0 +1,63 @@
+import "./SearchListing.css";
+
+export default function SearchListing() {
+  return (
+    <>
+      <section id="search-listing-section" className="main-section framed-box">
+        <div className="listing-wrapper">
+          <svg
+            width="24px"
+            height="24px"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            color="#000000"
+          >
+            <path
+              d="M12 11.5V16.5"
+              stroke="#000000"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M12 7.51L12.01 7.49889"
+              stroke="#000000"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+              stroke="#000000"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <div className="listing-body">
+            <p>You are looking at shops around</p>
+            <p>158 Queen Street East, Toronto, Canada</p>
+            <p>About 40 shops were found in your search</p>
+          </div>
+        </div>
+        <form action="" id="hero-form" className="form-container">
+          <div className="form-container__item">
+            <input type="text" placeholder="Enter your address" />
+          </div>
+          <div className="form-container__item custom-select">
+            <select name="" id="select-container">
+              <option value="" disabled selected>
+                Choose a category
+              </option>
+              <option value="">Sales</option>
+              <option value="">Repairs</option>
+            </select>
+          </div>
+          <button>Find shops</button>
+        </form>
+      </section>
+    </>
+  );
+}
