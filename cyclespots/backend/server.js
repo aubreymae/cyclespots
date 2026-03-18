d07fdc26fc8ee415cd3d588e98bcb7a7383115e3
Aubrey Garcia <aubreymaerg@gmail.com>
2026-03-17 20:12:53 -0400

    Created server file to start express

---
+++ cyclespots/backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import router from "./routes/storeRoute.js";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use("/posts", router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}/`);
+});
