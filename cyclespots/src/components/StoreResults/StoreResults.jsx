d46617be49b0df7be5266a3329a79ffd1d8b4448
Aubrey Garcia <aubreymaerg@gmail.com>
2026-03-23 11:03:45 -0400

    Fixed store listings table styling

--- cyclespots/src/components/StoreResults/StoreResults.jsx
+++ cyclespots/src/components/StoreResults/StoreResults.jsx
@@ -21,29 +21,29 @@ function Results() {
 
   return (
     <>
-      <section id="store-results-section" className="main-section framed-box">
+      <section id="store-results-section" className="framed-box">
         <table id="store-results-tb">
           <thead>
             <tr className="store-results-tb__h">
-              <th>Index</th>
-              <th>Store Name</th>
-              <th>Address</th>
-              <th>Services</th>
-              <th>Rating</th>
+              <th className="th-index">Index</th>
+              <th className="th-store-name">Store Name</th>
+              <th className="th-address">Address</th>
+              <th className="th-services">Services</th>
+              <th className="th-rating">Rating</th>
             </tr>
           </thead>
           <tbody>
             {stores.map((store) => (
               <tr key={store.store_id} className="store-results-tb__h">
-                <td>{store.store_id}</td>
-                <td>{store.store_name}</td>
-                <td>{store.street_address}</td>
-                <td>
+                <td className="th-index">{store.store_id}</td>
+                <td className="th-store-name">{store.store_name}</td>
+                <td className="th-address">{store.street_address}</td>
+                <td className="th-services">
                   {store.store_services
                     ?.map((ss) => ss.services.service_name)
                     .join(", ") || " "}
                 </td>
-                <td>{store.rating}</td>
+                <td className="th-rating">{store.rating}</td>
               </tr>
             ))}
           </tbody>
