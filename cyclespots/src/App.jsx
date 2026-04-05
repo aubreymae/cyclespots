0ad4b565aa2735f022bdf31e535a0678fc969253
Aubrey Garcia <aubreymaerg@gmail.com>
2026-04-05 18:53:19 -0400

    Added route for store details to render individual store pages

--- cyclespots/src/App.jsx
+++ cyclespots/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/listings" element={<Listings />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/stores/:slug" element={<StoreDetail />} />
         </Route>
       </Routes>
     </>
