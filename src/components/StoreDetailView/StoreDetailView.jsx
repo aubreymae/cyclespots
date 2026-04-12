0aab767f69897a6b6d69e02121dc915d3bfe117f
Aubrey Garcia <aubreymaerg@gmail.com>
2026-04-12 15:11:33 -0400

    Updated individual store listing page to include new table column data

--- src/components/StoreDetailView/StoreDetailView.jsx
+++ src/components/StoreDetailView/StoreDetailView.jsx
@@ -27,15 +27,52 @@ export default function StoreDetail({ slug }) {
     <>
       <section className="store-container main-section framed-box">
         <div className="store-title-container">
-          <h1>{store.store_name}</h1>
-          <span className="store-rating-bubble">{store.rating}</span>
-          {store.store_services?.map((s) => (
-            <span className="store-service-bubble" key={s.id}>
-              {s.services.service_name}
-            </span>
-          ))}
+          <div className="store-title-wrapper">
+            <h1>{store.store_name}</h1>
+            <span className="store-rating-bubble">{store.rating}</span>
+          </div>
+          <div className="store-services-wrapper">
+            <span className="services-list-title">Services offered:</span>
+            {store.store_services?.map((s) => (
+              <span className="services-list-item" key={s.id}>
+                {s.services.service_name}
+              </span>
+            ))}
+          </div>
         </div>
+      </section>
+      <section className="store-container main-section framed-box">
+        <h2>Street Address</h2>
         <p>{store.street_address}</p>
+      </section>
+      <section className="two-col-grid">
+        <div className="grid-col grid-col-1">
+          <section className="store-container main-section framed-box">
+            <h2>Phone</h2>
+            <p>{store.phone}</p>
+          </section>
+          <section className="store-container main-section framed-box">
+            <h2>Store Hours</h2>
+          </section>
+        </div>
+        <div className="grid-col grid-col-2">
+          <section className="store-container main-section framed-box">
+            <h2>Website</h2>
+            <a href={store.website} target="_blank" rel="noopener noreferrer">
+              {store.website}
+            </a>
+          </section>
+          <section className="store-container main-section framed-box">
+            <h2>Maps</h2>
+            <iframe
+              src={store.google_maps_url}
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              className="google-maps-container"
+            ></iframe>
+          </section>
+        </div>
       </section>
     </>
   );
